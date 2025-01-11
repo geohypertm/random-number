14e83d50d5017afb5435c23f47b6038eb18b54e3
AmirBiG <itsamirbig@gmail.com>
2025-01-11 20:08:58 +0330

    practice tailwindcss border

--- src/Components/Practice-Tailwind.jsx
+++ src/Components/Practice-Tailwind.jsx
@@ -1,11 +1,12 @@
 const PracticeTailwind = () => {
     return (
-        <div className="mt-5">   
-        <p className="bg-red-400 p-5 m-2 rounded-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, eius.</p>
-        <p className="bg-orange-400 p-5 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, eius.</p>
-        <p className="bg-teal-300 p-5 m-2 rounded-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, eius.</p>
-        <p className="bg-blue-300 p-5 m-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, eius.</p>
+        <>
+        <div id="alert" className="flex flex-row bg-red-300 p-4 w-[20%] justify-between">
+            <span>Lorem ipsum dolor sit amet.
+            </span>
+            <button className="pt-[-10px]" data-dismiss-target="#alert">x</button>
         </div>
+        </>
     )
 }
 
