958746320c6c7e15b33457e8e85ddba6a3f54fa0
AmirBiG <itsamirbig@gmail.com>
2025-01-11 17:15:19 +0330

    changed components to class

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,10 +1,23 @@
-const Counter = () => {
+import { Component } from "react";
 
-    return (
-        <>
-        <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">شمارنده : {Math.floor(Math.random() * 1000)}</p>
-        </>
-    )
+
+class Counter extends Component {
+    render() {
+        return (
+            <>
+            <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">شمارنده : {Math.floor(Math.random() * 1000)}</p>
+            </>
+        )
+    }
 }
+
+// const Counter = () => {
+
+//     return (
+//         <>
+//         <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">شمارنده : {Math.floor(Math.random() * 1000)}</p>
+//         </>
+//     )
+// }
 
 export default Counter;
--- src/Geohyper.js
+++ src/Geohyper.js
@@ -1,17 +1,34 @@
+import { Component } from "react";
+
 import Counter from "./Components/Counter";
 
-import "./index.css"
+import "./index.css";
 import "./Geohyper.css";
 
-const Geohyper = () => { 
+class Geohyper extends Component {
+  render() {
     return (
-       <div>
-         <h1 className="bg-blue-100 text-blue-800 text-[40px] font-medium p-1 m-2 text-center rounded-md hover:bg-blue-200 transition duration-[0.5s] cursor-default select-none w-[20%] mx-auto">شمارنده من</h1>
-         <hr className="h-[1.5px] text-center w-full rounded-mdS my-8 mx-auto bg-fuchsia-400 border-0 hover:w-5 transition-all duration-[3s]"/>
+      <div>
+        <h1 className="bg-blue-100 text-blue-800 text-[40px] font-medium p-1 m-2 text-center rounded-md hover:bg-blue-200 transition duration-[0.5s] cursor-default select-none w-[20%] mx-auto">
+          شمارنده من
+        </h1>
+        <hr className="h-[1.5px] text-center w-full rounded-mdS my-8 mx-auto bg-fuchsia-400 border-0 hover:w-5 transition-all duration-[3s]" />
 
-         <Counter />
-       </div>
-    )
+        <Counter />
+      </div>
+    );
+  }
 }
+
+// const Geohyper = () => {
+//     return (
+//        <div>
+//          <h1 className="bg-blue-100 text-blue-800 text-[40px] font-medium p-1 m-2 text-center rounded-md hover:bg-blue-200 transition duration-[0.5s] cursor-default select-none w-[20%] mx-auto">شمارنده من</h1>
+//          <hr className="h-[1.5px] text-center w-full rounded-mdS my-8 mx-auto bg-fuchsia-400 border-0 hover:w-5 transition-all duration-[3s]"/>
+
+//          <Counter />
+//        </div>
+//     )
+// }
 
 export default Geohyper;
