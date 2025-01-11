5604826e1c4a09cafb0715a746db0f2f42e282ed
GeoHyper <geohypertm@gmail.com>
2025-01-11 13:11:47 +0330

    change againg language to default

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,7 +2,7 @@ const Counter = () => {
 
     return (
         <>
-        <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">Counter: {Math.floor(Math.random() * 1000)}</p>
+        <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">شمارنده : {Math.floor(Math.random() * 1000)}</p>
         </>
     )
 }
