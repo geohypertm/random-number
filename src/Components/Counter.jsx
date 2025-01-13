8ff7d06d2b43c6873c3da49fe18e1cb956792a96
AmirBiG <itsamirbig@gmail.com>
2025-01-13 21:40:35 +0330

    practice shadow tailwindcss

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -20,6 +20,8 @@ class Counter extends Component {
         <br />
         <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">{number}</p>
         <p className="text-center m-auto bg-blue-100 text-stone-500 font-medium text-[40px] rounded-md w-[20%] p-2">{count}</p>
+
+        <div className="shadow-xl shadow-green-400 p-12 m-5">Lorem ipsum dolor sit amet consectetur.</div>
       </>
     );
   }
